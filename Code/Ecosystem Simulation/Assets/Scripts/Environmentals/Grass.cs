5fbed89b4ae7de9178a177c826326a3259216305
Ruari <ruari.armstrong@gmail.com>
2020-10-13 15:15:04 +0100

    Add grass script

---
+++ Code/Ecosystem Simulation/Assets/Scripts/Environmentals/Grass.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+    private int currentHeight;
+    private static int maxHeight = 4; // max height to be shared by all grass
+    private static int baseEnergy = 20; // base energy to be shared by all grass
+
+    // Default Grass constructor
+    Grass()
+    {
+        currentHeight = 0;
+    }
+
+    // Grass constructor with specifed current height
+    Grass(int height)
+    {
+        currentHeight = height;
+    }
+
+    // Setter for Max Height of grass
+    void SetMaxHeight(int height)
+    {
+        maxHeight = height;
+    }
+
+    // Energy is a property that multiplies baseEnergy with grass height
+    // to give the energy when eaten
+    public int Energy
+    {
+        get
+        {
+            return baseEnergy * currentHeight;
+        }
+        set
+        {
+            baseEnergy = value / currentHeight;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
