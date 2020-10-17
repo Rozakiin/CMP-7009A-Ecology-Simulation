9c4241db1fb859ede53dfb61d07a42e5c438b9cd
“chenzhony” <czy89897@gmail.com>
2020-10-17 21:30:37 +0100

    Add Animal class add get and set method in fox class and rabbit class

--- Code/Ecosystem Simulation/Assets/Scripts/Animals/Animal.cs
+++ Code/Ecosystem Simulation/Assets/Scripts/Animals/Animal.cs
@@ -10,6 +10,7 @@ public abstract class Animal : MonoBehaviour
     protected float reproUrge;
     protected float rabEatTime = 1;
     protected float pregnaTime = 5;
+    protected float sightRadius;
     protected abstract float maxLifeExpe { get; set;}
     protected abstract float babyNum { get; set;}
     protected Animal()
