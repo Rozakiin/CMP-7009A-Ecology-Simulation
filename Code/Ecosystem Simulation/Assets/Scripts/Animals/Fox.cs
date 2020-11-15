2d705b3b7abab659b4077d8d842064ff1d8f5711
Ruari <ruari.armstrong@gmail.com>
2020-11-15 10:45:01 +0000

    Add default values for fox properties

--- Code/Ecosystem Simulation/Assets/Scripts/Animals/Fox.cs
+++ Code/Ecosystem Simulation/Assets/Scripts/Animals/Fox.cs
@@ -4,18 +4,75 @@ using UnityEngine;
 
 public class Fox : Animal
 {
-    private static float moveSpeedBase;
+    #region Defaults
+    public class DefaultValues
+    {
+        public static readonly float moveSpeed = 25f;
+        public static readonly float scaleMale = 3f;
+        public static readonly float scaleFemale = 3f;
+        public static readonly float hungerMax = 100f;
+        public static readonly float thirstMax = 100f;
+        public static readonly int ageMax = 800;
+        public static readonly float pregnancyLength = 20f;
+    }
+    #endregion
+
+    #region Properties
+    private static float moveSpeedBase = DefaultValues.moveSpeed;
     public override float MoveSpeed
     {
         get { return moveSpeedBase*moveMultiplier; }
         protected set { moveSpeedBase = value; }
     }
 
-    protected override int LitterSizeMax   // overriding property
+    private static float hungerMax = DefaultValues.hungerMax;
+    public override float HungerMax
+    {
+        get { return hungerMax; }
+        protected set { hungerMax = value; }
+    }
+
+    private static float thirstMax = DefaultValues.thirstMax;
+    public override float ThirstMax
+    {
+        get { return thirstMax; }
+        protected set { thirstMax = value; }
+    }
+
+    private static int ageMax = DefaultValues.ageMax;
+    public override int AgeMax
+    {
+        get { return ageMax; }
+        protected set { ageMax = value; }
+    }
+
+    protected override int LitterSizeMax
     {
         get { return LitterSizeMax; }
-        set {}
+        set { }
     }
+
+    private static float pregnancyLengthBase = DefaultValues.pregnancyLength;
+    public override float PregnancyLength
+    {
+        get { return pregnancyLengthBase * pregnancyLengthModifier; }
+        protected set { pregnancyLengthBase = value; }
+    }
+
+    private static float scaleFemaleBase = DefaultValues.scaleFemale;
+    protected override float ScaleFemale
+    {
+        get { return scaleFemaleBase * scaleMultiplier; }
+        set { scaleFemaleBase = value; }
+    }
+
+    private static float scaleMaleBase = DefaultValues.scaleMale;
+    protected override float ScaleMale
+    {
+        get { return scaleMaleBase * scaleMultiplier; }
+        set { scaleMaleBase = value; }
+    }
+    #endregion
 
     #region Initialisation
     // Start is called before the first frame update
