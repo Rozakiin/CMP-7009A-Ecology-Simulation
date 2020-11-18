950766f0069e63bbeadeca766795a83b9788766a
Ruari <ruari.armstrong@gmail.com>
2020-11-18 09:31:20 +0000

    Add method to a number spawn entitys from a given gameobject

--- Code/Ecosystem-ECS/Assets/Scripts/SimulationManager.cs
+++ Code/Ecosystem-ECS/Assets/Scripts/SimulationManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
 using Unity.Mathematics;
@@ -25,21 +25,7 @@ public class SimulationManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Create entity prefab from the game object hierarchy once
-        var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
-        var grassEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(grass, settings);
-        var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-
-
-        for (int i = 0; i < numberOfRabbitsToSpawn; i++)
-        {
-            // Efficiently instantiate a bunch of entities from the already converted entity prefab
-            var instance = entityManager.Instantiate(grassEntity);
-
-            // Place the instantiated entity in a grid with some noise
-            var position = transform.TransformPoint(new float3(i, noise.cnoise(new float2(i) * 0.21F) * 2, i * 1.3F));
-            entityManager.SetComponentData(instance, new Translation { Value = position });
-        }
+        CreateEntitiesFromGameObject(grass, numberOfGrassToSpawn);
     }
 
     // Update is called once per frame
@@ -47,4 +33,32 @@ public class SimulationManager : MonoBehaviour
     {
         
     }
+
+    private bool CreateMap()
+    {
+        return true;
+    }
+
+    // Creates entities from a gameobject in a given quantity
+    private void CreateEntitiesFromGameObject(GameObject gameObject, int quantity)
+    {
+        // Create entity prefab from the game object hierarchy once
+        var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+        var entity = GameObjectConversionUtility.ConvertGameObjectHierarchy(gameObject, settings);
+        var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+
+        for (int i = 0; i < quantity; i++)
+        {
+            // Efficiently instantiate a bunch of entities from the already converted entity prefab
+            var instance = entityManager.Instantiate(entity);
+
+            // Place the instantiated entity in a grid with some noise
+            var position = transform.TransformPoint(new float3(i, noise.cnoise(new float2(i) * 0.21F) * 2, i * 1.3F));
+            //Set Component Data for the entity
+            entityManager.SetComponentData(instance, new Translation { Value = position }); // set position data (called translation in ECS)
+        }
+    }
+
+
 }
