eb57a15a5642963e3cb7339397ca9b9bd051a1ff
Ruari <ruari.armstrong@gmail.com>
2020-11-24 14:05:29 +0000

    Add rabbitdefaults static class

---
+++ Code/Ecosystem-ECS/Assets/Scripts/EntityDefaults/RabbitDefaults.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+//Use this class to define all the default values for components related to Rabbit Entities
+public static class RabbitDefaults
+{
+    //Age
+    public static float age = 0f;
+    public static float ageIncrease = 0.2f;
+    public static float ageMax = 600f;
+
+    //Drinkable
+
+
+    //Edible
+    public static float nutritionalValue = 10f;
+    public static bool canBeEaten = true;
+    public static float nutritionalValueMultiplier = 1f;
+    public static EdibleData.FoodType foodType = EdibleData.FoodType.Meat;
+
+    //Gender
+
+
+    //Hunger
+    public static float hunger = 0f;
+    public static float hungerMax = 100f;
+    public static float hungryThreshold = 20f;
+    public static float hungerIncrease = 0.5f;
+    public static float eatingSpeed = 1f;
+    public static Entity entityToEat = Entity.Null;
+    public static HungerData.Diet diet = HungerData.Diet.Herbivore;
+
+    //Mate
+    public static float mateStartTime = 0f;
+    public static float matingDuration = 5f;
+    public static float reproductiveUrge = 0f;
+    public static float reproductiveUrgeIncrease = 1f;
+    public static float matingThreshold = 50f;
+    public static Entity entityToMate = Entity.Null;
+
+    //Movement
+    public static float moveSpeed = 25f;
+    public static float rotationSpeed = 10f;
+    public static float moveMultiplier = 1f;
+
+    //Pregnancy
+    public static float pregnancyStartTime = 0f;
+    public static bool pregnant = false;
+    public static int babiesBorn = 0;
+    public static float birthStartTime = 0f;
+    public static int currentLitterSize = 0;
+    public static float pregnancyLengthModifier = 1f;
+    public static float pregnancyLength = 10f;
+    public static float birthDuration = 10f;
+    public static int litterSizeMin = 1;
+    public static int litterSizeMax = 13;
+    public static int litterSizeAve = 7;
+
+    //Size
+    public static float sizeMultiplier = 1f;
+    public static float scaleMale = 2f;
+    public static float scaleFemale = 3f;
+
+    //State
+    public static StateData.States state = StateData.States.Wandering;
+    public static StateData.States previousState = StateData.States.Wandering;
+    public static StateData.DeathReason deathReason = StateData.DeathReason.Eaten;
+    public static bool beenEaten = false;
+
+    //Target
+
+
+    //TerrainType
+
+
+    //Thirst
+    public static float thirst = 0f;
+    public static float thirstMax = 100f;
+    public static float thirstyThreshold = 10f;
+    public static float thirstIncrease = 0.5f;
+    public static float drinkingSpeed = 2f;
+    public static Entity entityToDrink = Entity.Null;
+
+    //Vision
+    public static float touchRadius = 1f;
+    public static float sightRadius = 20f;
+}
