6eafad7f6bf6145200eefc2bce15e54d8e46c3f1
chenzhony <czy89897@gmail.com>
2020-11-29 18:31:19 +0000

    New fastforward to ecs (#30)
    * * TimeControlSystem.cs: Add fastforward system to ECS
    
    * Ecosystem-ECS.unity: Add UI
    
    * Add UI
    
    * Delete .gitignore
    
    * Delete CMP-7009A-Ecology-Simulation.iml
    
    * Delete misc.xml
    
    * Delete modules.xml
    
    * Delete vcs.xml

--- Code/Ecosystem-ECS/Assets/Scripts/MonoBehaviourTools/UI/TimeControlSystem/TimeControlSystem.cs
+++ Code/Ecosystem-ECS/Assets/Scripts/MonoBehaviourTools/UI/TimeControlSystem/TimeControlSystem.cs
@@ -8,10 +8,21 @@ public class TimeControlSystem : MonoBehaviour
     public Text speedDisplay;
     public float fastForwardMultiplier;
     bool pause;
-
+    public Button PlayButton;
+    public Button PauseButton;
+    public Button SlowButton;
+    public Button DownButton;
     void Start()
     {
         speedDisplay.text = fastForwardMultiplier.ToString();
+        Button PlayButton1 = PlayButton.GetComponent<Button>();
+        PlayButton1.onClick.AddListener(Play);
+        Button PauseButton1 = PauseButton.GetComponent<Button>();
+        PauseButton1.onClick.AddListener(Pause);
+        Button SlowButton1 = SlowButton.GetComponent<Button>();
+        SlowButton1.onClick.AddListener(IncreaseSpeed);
+        Button DownButton1 = DownButton.GetComponent<Button>();
+        DownButton1.onClick.AddListener(DecreaseSpeed);
     }
     void Update()
     {
@@ -19,7 +30,7 @@ public class TimeControlSystem : MonoBehaviour
         {
             Time.timeScale = 0f;
         }
-        if (fastForwardMultiplier < 0)
+        if (fastForwardMultiplier < 0 || fastForwardMultiplier>100)
         {
             fastForwardMultiplier = 0f;
         }
@@ -49,12 +60,12 @@ public class TimeControlSystem : MonoBehaviour
     }
     public void DecreaseSpeed()
     {
-        if (fastForwardMultiplier <= 1f && fastForwardMultiplier>0.2f)
+        if (fastForwardMultiplier <= 1f && fastForwardMultiplier > 0.2f)
         {
             fastForwardMultiplier -= 0.2f;
             Time.timeScale = pause ? 0f : fastForwardMultiplier;
         }
-        else if(fastForwardMultiplier > 1f)
+        else if (fastForwardMultiplier > 1f)
         {
             fastForwardMultiplier -= 1f;
             Time.timeScale = pause ? 0f : fastForwardMultiplier;
