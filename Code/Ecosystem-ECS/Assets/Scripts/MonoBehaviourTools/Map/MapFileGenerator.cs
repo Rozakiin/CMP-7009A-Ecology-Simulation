8d55b1753441f8bded2e4cd94fddfb3ee673cc39
Ruari <ruari.armstrong@gmail.com>
2020-12-01 12:45:06 +0000

    Update MapFileGenerator to work in the menu

--- Code/Ecosystem-ECS/Assets/Scripts/MonoBehaviourTools/Map/MapFileGenerator.cs
+++ Code/Ecosystem-ECS/Assets/Scripts/MonoBehaviourTools/Map/MapFileGenerator.cs
@@ -8,201 +8,266 @@ using UnityEngine.UI;
 public class MapFileGenerator : MonoBehaviour
 {
 
-	public int width;
-	public int height;
+    public int width;
+    public int height;
 
-	public string seed;
-	public bool useRandomSeed;
-	public Button MapGenerator;
-	public Button FileGenerator;
+    public string seed;
+    public bool useRandomSeed;
+    public Button mapGeneratorButton;
+    public Button saveFileButton;
+    public Slider widthSlider;
+    public Slider heightSlider;
+    public Slider waterFillSlider;
+    public Text widthSliderText;
+    public Text heightSliderText;
+    public Text waterFillSliderText;
+    public InputField seedInput;
 
-	[Range(0, 100)]
-	public int randomFillPercent;
+    [Range(0, 100)]
+    public int waterFillPercent;
 
-	public MapFileGenerator(int randomFillPercent)
-	{
-		this.randomFillPercent = randomFillPercent;
-	}
+    int[,] map;
 
-	int[,] map;
-	void Start()
-	{
-		GenerateMap("seed");
-		//FileGenerate();
-		Button btn = MapGenerator.GetComponent<Button>();
-		btn.onClick.AddListener(TaskOnClick);
-		Button btn1 = FileGenerator.GetComponent<Button>();
-		btn1.onClick.AddListener(FileGenerate);
-	}
-	void TaskOnClick()
-	{
-		if (useRandomSeed == true)
-		{
-			seed = DateTime.Now.ToString();
-			GenerateMap(seed);
-		}
-		else
-		{
-			GenerateMap(seed);
-		}
-	}
+    //Tile Sprite rendering
+    public Texture2D grassTex;
+    public Texture2D waterTex;
+    public Texture2D sandTex;
+    public Texture2D rockTex;
 
-	void FileGenerate()
-	{
+    public GameObject tilePrefab;
+    public GameObject[,] tiles;
+
+
+    void Start()
+    {
+        //GenerateMap("seed");
+        mapGeneratorButton.onClick.AddListener(TaskOnClick);
+        saveFileButton.onClick.AddListener(FileGenerate);
+
+        widthSlider.onValueChanged.AddListener(SetWidth);
+        heightSlider.onValueChanged.AddListener(SetHeight);
+        waterFillSlider.onValueChanged.AddListener(SetWaterFillPercent);
+
+        seedInput.onEndEdit.AddListener(SetSeed);
+    }
+
+    private void SetSeed(string value)
+    {
+        seed = value;
+    }
+
+    private void SetWaterFillPercent(float value)
+    {
+        waterFillPercent = (int)value;
+        waterFillSliderText.text = value.ToString();
+    }
+
+    private void SetHeight(float value)
+    {
+        height = (int)value;
+        heightSliderText.text = value.ToString();
+    }
+
+    private void SetWidth(float value)
+    {
+        width = (int)value;
+        widthSliderText.text = value.ToString();
+    }
+
+    void TaskOnClick()
+    {
+        if (useRandomSeed == true)
+        {
+            seed = DateTime.Now.ToString();
+            GenerateMap(seed);
+        }
+        else
+        {
+            GenerateMap(seed);
+        }
+    }
+
+    void FileGenerate()
+    {
         Debug.Log("File Generate Yeah!");
-		string file = Application.dataPath;
-		string fileName = file + "/Scripts/Map/Map.txt";
-		try
-		{
-			// Check if file already exists. If yes, delete it.     
-			if (File.Exists(fileName))
-			{
-				File.Delete(fileName);
-			}
+        string file = Application.dataPath;
+        string fileName = file + "/Scripts/Map/Map.txt";
+        try
+        {
+            // Check if file already exists. If yes, delete it.     
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
 
-			// Create a new file     
-			using (StreamWriter sw = File.CreateText(fileName))
-			{
-				string output = "";
-				for (int i = 0; i < map.GetUpperBound(0); i++)
-				{
-					for (int j = 0; j < map.GetUpperBound(1); j++)
-					{
-						output += map[i, j].ToString();
-					}
-					sw.WriteLine(output);
-					output = "";
-				}
-				sw.Close();
-			}
-		}
-		catch (Exception Ex)
-		{
-			Console.WriteLine(Ex.ToString());
-		}
-	}
-
-
-	void GenerateMap(string seed)
-	{
-		map = new int[width, height];
-		
-		RandomFillMap(seed);
-
-		for (int i = 0; i < 5; i++)
-		{
-			SmoothMap();
-			CreateMoreTile();
-		}
-
-	}
-
-	void CreateMoreTile()
-	{
-		for (int i = 1; i < width; i++)
-		{
-			for (int j = 1; j < height - 1; j++)
-			{
-				if (map[i, j] == 0)
-				{
-					map[i - 1, j] = (map[i - 1, j] == 0) ? 0 : 2;
-					map[i + 1, j] = (map[i + 1, j] == 0) ? 0 : 2;
-					map[i, j + 1] = (map[i, j + 1] == 0) ? 0 : 2;
-					map[i, j - 1] = (map[i, j - 1] == 0) ? 0 : 2;
-				}
-			}
-		}
-	}
+            // Create a new file     
+            using (StreamWriter sw = File.CreateText(fileName))
+            {
+                string output = "";
+                for (int i = 0; i < map.GetUpperBound(0); i++)
+                {
+                    for (int j = 0; j < map.GetUpperBound(1); j++)
+                    {
+                        output += map[i, j].ToString();
+                    }
+                    sw.WriteLine(output);
+                    output = "";
+                }
+                sw.Close();
+            }
+        }
+        catch (Exception Ex)
+        {
+            Console.WriteLine(Ex.ToString());
+        }
+    }
 
 
-	void RandomFillMap(string seed)
-	{
+    void GenerateMap(string seed)
+    {
+        map = new int[width, height];
+        
+        RandomFillMap(seed);
 
-		System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+        for (int i = 0; i < 5; i++)
+        {
+            SmoothMap();
+            CreateMoreTile();
+        }
 
-		for (int x = 0; x < width; x++)
-		{
-			for (int y = 0; y < height; y++)
-			{
-				if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
-				{
-					map[x, y] = 1;
-				}
-				else
-				{
-					map[x, y] = (pseudoRandom.Next(0, 100) < randomFillPercent) ? 1 : 0;
-				}
-			}
-		}
-	}
+        DrawTiles();
+    }
 
-	void SmoothMap()
-	{
-		for (int x = 0; x < width; x++)
-		{
-			for (int y = 0; y < height; y++)
-			{
-				int neighbourWallTiles = GetSurroundingWallCount(x, y);
-
-				if (neighbourWallTiles > 4)
-					map[x, y] = 1;
-				else if (neighbourWallTiles < 4)
-					map[x, y] = 0;
-
-			}
-		}
-	}
-
-	int GetSurroundingWallCount(int gridX, int gridY)
-	{
-		int wallCount = 0;
-		for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++)
-		{
-			for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
-			{
-				if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height)
-				{
-					if (neighbourX != gridX || neighbourY != gridY)
-					{
-						wallCount += map[neighbourX, neighbourY];
-					}
-				}
-				else
-				{
-					wallCount++;
-				}
-			}
-		}
-
-		return wallCount;
-	}
+    void CreateMoreTile()
+    {
+        for (int i = 1; i < width; i++)
+        {
+            for (int j = 1; j < height - 1; j++)
+            {
+                if (map[i, j] == 0)
+                {
+                    map[i - 1, j] = (map[i - 1, j] == 0) ? 0 : 2;
+                    map[i + 1, j] = (map[i + 1, j] == 0) ? 0 : 2;
+                    map[i, j + 1] = (map[i, j + 1] == 0) ? 0 : 2;
+                    map[i, j - 1] = (map[i, j - 1] == 0) ? 0 : 2;
+                }
+            }
+        }
+    }
 
 
-	void OnDrawGizmos()
-	{
-		if (map != null)
-		{
-			for (int x = 0; x < width; x++)
-			{
-				for (int y = 0; y < height; y++)
-				{
-					if (map[x, y] == 1)
-					{
-						Gizmos.color = Color.green;
-					}
-					if (map[x, y] == 0)
-					{
-						Gizmos.color = Color.blue;
-					}
-					if (map[x, y] == 2)
-					{
-						Gizmos.color = Color.black;
-					}
-					Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
-					Gizmos.DrawCube(pos, Vector3.one);
-				}
-			}
-		}
-	}
+    void RandomFillMap(string seed)
+    {
 
+        System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                {
+                    map[x, y] = 1;
+                }
+                else
+                {
+                    map[x, y] = (pseudoRandom.Next(0, 100) < waterFillPercent) ? 1 : 0;
+                }
+            }
+        }
+    }
+
+    void SmoothMap()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int neighbourWallTiles = GetSurroundingWallCount(x, y);
+
+                if (neighbourWallTiles > 4)
+                    map[x, y] = 1;
+                else if (neighbourWallTiles < 4)
+                    map[x, y] = 0;
+
+            }
+        }
+    }
+
+    int GetSurroundingWallCount(int gridX, int gridY)
+    {
+        int wallCount = 0;
+        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++)
+        {
+            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
+            {
+                if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height)
+                {
+                    if (neighbourX != gridX || neighbourY != gridY)
+                    {
+                        wallCount += map[neighbourX, neighbourY];
+                    }
+                }
+                else
+                {
+                    wallCount++;
+                }
+            }
+        }
+
+        return wallCount;
+    }
+
+
+    void DrawTiles()
+    {
+        if (tiles == null)
+        {
+            tiles = new GameObject[width, height];
+        }
+        if (map != null && tiles != null)
+        {
+            // Destroy old gameobjects
+            foreach (GameObject tile in tiles)
+            {
+                Destroy(tile);
+            }
+            tiles = new GameObject[width, height];
+
+            float tileWidth = tilePrefab.GetComponent<RectTransform>().rect.width;
+            float tileHeight = tilePrefab.GetComponent<RectTransform>().rect.height;
+
+            Vector3 pos;
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    pos = new Vector3(x*tileWidth, y*tileHeight , 0);
+                    tiles[x, y] = Instantiate(tilePrefab); //Instantiate tile
+                    tiles[x,y].transform.SetParent(this.transform); // set parent as this
+                    tiles[x, y].transform.position = pos; // set position
+                    // change colour based on value
+                    switch (map[x, y])
+                    {
+                        case 0:
+                            tiles[x, y].GetComponent<Image>().color = Color.blue;
+                            break;
+                        case 1:
+                            tiles[x, y].GetComponent<Image>().color = Color.green;
+                            break;
+                        case 2:
+                            tiles[x, y].GetComponent<Image>().color = Color.yellow;
+                            break;
+                        case 3:
+                            tiles[x, y].GetComponent<Image>().color = Color.grey;
+                            break;
+                        default:
+                            tiles[x, y].GetComponent<Image>().color = Color.black;
+                            break;
+                    }
+                }
+            }
+        }
+    }
 }
