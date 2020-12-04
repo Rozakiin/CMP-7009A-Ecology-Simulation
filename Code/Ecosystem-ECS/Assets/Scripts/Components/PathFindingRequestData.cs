85ca5f052c05250f0da5adc9e87e6e89c883b861
Ruari <ruari.armstrong@gmail.com>
2020-12-04 15:10:19 +0000

    Add PathFindingRequestData component
    gives start and end position in world coords

---
+++ Code/Ecosystem-ECS/Assets/Scripts/Components/PathFindingRequestData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+public struct PathFindingRequestData : IComponentData
+{
+    public float3 startPosition;
+    public float3 endPosition;
+}
