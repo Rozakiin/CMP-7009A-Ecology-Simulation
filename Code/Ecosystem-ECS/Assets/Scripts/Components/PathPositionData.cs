61f6675e56d09dbdc17aa9a2f38e66698292015b
Ruari <ruari.armstrong@gmail.com>
2020-12-04 15:11:48 +0000

    Add path following components
    pathfollow stores the current index of the path to follow, path position contains the world position of the path node, path position data is used to add the path position to a dynamic buffer

---
+++ Code/Ecosystem-ECS/Assets/Scripts/Components/PathPositionData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[InternalBufferCapacity(200)] //gives max capactity for the buffer
+public struct PathPositionData : IBufferElementData
+{
+    public float3 position; //world position of path node
+}
