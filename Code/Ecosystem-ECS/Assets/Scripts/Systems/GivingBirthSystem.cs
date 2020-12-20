3b339f305d9dbd8d6340c874257d4521ae951ff2
Lukasz <dzi.lukasz@gmail.com>
2020-12-20 02:20:19 +0000

    Spawning children
    Spawning children may be possible now, needs testing with LookingSystem. Not sure about updating values, hence the testing. Would be nice to have a way of using some sort of a generic class so that spawning children doesn't have to be copy-pasted for Foxes and future animals.

--- Code/Ecosystem-ECS/Assets/Scripts/Systems/GivingBirthSystem.cs
+++ Code/Ecosystem-ECS/Assets/Scripts/Systems/GivingBirthSystem.cs
@@ -24,7 +24,8 @@ public class GivingBirthSystem : SystemBase
             ref ReproductiveData reproductiveData,
             ref StateData stateData,
             ref MovementData movementData,
-            in BioStatsData bioStatsData
+            in BioStatsData bioStatsData,
+            in Translation translation
             ) => {
             
                 if(stateData.state == StateData.States.GivingBirth)
@@ -35,13 +36,84 @@ public class GivingBirthSystem : SystemBase
                         ////give birth
                         //Entity newEntity = e;
                         //EntityManager.Instantiate(e);
+
                         
                         //ArchetypeChunkEntityType archetype =  this.GetArchetypeChunkEntityType();
                         Entity newEntity = ecb.Instantiate(entityInQueryIndex, e);
-                        /*ecb.SetComponent(entityInqueryIndex, newEntity, new StateData
-                        {
-                            //
-                        })*/
+                        ecb.SetComponent(entityInQueryIndex, newEntity, 
+                            new StateData
+                            {
+                                state = RabbitDefaults.state,
+                                previousState = RabbitDefaults.previousState
+                            }
+                        );
+
+                        ecb.SetComponent(entityInQueryIndex, newEntity,
+                            new TargetData
+                            {
+                                atTarget = true,
+                                currentTarget = translation.Value,
+                                oldTarget = translation.Value
+                            }
+                        );
+
+                        ecb.SetComponent(entityInQueryIndex, newEntity,
+                            new PathFollowData
+                            {
+                                pathIndex = -1
+                            }
+                        );
+
+                        ecb.SetComponent(entityInQueryIndex, newEntity,
+                            new BasicNeedsData
+                            {
+                                hunger = RabbitDefaults.hunger,
+                                hungerIncrease = RabbitDefaults.hungerIncrease,
+                                entityToEat = RabbitDefaults.entityToEat,
+                                diet = RabbitDefaults.diet,
+                                thirst = RabbitDefaults.thirst,
+                                thirstIncrease = RabbitDefaults.thirstIncrease,
+                                drinkingSpeed = RabbitDefaults.drinkingSpeed,
+                                entityToDrink = RabbitDefaults.entityToDrink
+                            }
+                        );
+
+                        BioStatsData.Gender randGender = UnityEngine.Random.Range(0, 2) == 1 ? randGender = BioStatsData.Gender.Female : randGender = BioStatsData.Gender.Male;
+
+                        ecb.SetComponent(entityInQueryIndex, newEntity,
+                            new BioStatsData
+                            {
+                                age = RabbitDefaults.age,
+                                ageGroup = RabbitDefaults.ageGroup,
+                                gender = randGender
+                            }
+                        );
+
+                        ecb.SetComponent(entityInQueryIndex, newEntity,
+                            new ReproductiveData
+                            {
+                                mateStartTime = RabbitDefaults.mateStartTime,
+                                reproductiveUrge = RabbitDefaults.reproductiveUrge,
+                                reproductiveUrgeIncrease = (randGender == BioStatsData.Gender.Female ? RabbitDefaults.reproductiveUrgeIncreaseFemale : RabbitDefaults.reproductiveUrgeIncreaseMale),
+                                defaultRepoductiveIncrease = (randGender == BioStatsData.Gender.Female ? RabbitDefaults.reproductiveUrgeIncreaseFemale : RabbitDefaults.reproductiveUrgeIncreaseMale),
+                                entityToMate = RabbitDefaults.entityToMate,
+
+                                pregnant = RabbitDefaults.pregnant,
+                                babiesBorn = RabbitDefaults.babiesBorn,
+                                birthStartTime = RabbitDefaults.birthStartTime,
+                                currentLitterSize = RabbitDefaults.currentLitterSize,
+                                pregnancyStartTime = RabbitDefaults.pregnancyStartTime
+                            }
+                        );
+
+                        ecb.SetComponent(entityInQueryIndex, newEntity,
+                            new SizeData
+                            {
+                                size = (randGender == BioStatsData.Gender.Female ? RabbitDefaults.scaleFemale : RabbitDefaults.scaleMale),
+                                sizeMultiplier = RabbitDefaults.sizeMultiplier,
+                            }
+                        );
+
                         reproductiveData.birthStartTime = bioStatsData.age;
                         reproductiveData.babiesBorn++;
                     }
